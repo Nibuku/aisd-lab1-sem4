13caa02b868b83083a8f7de2f012fa72a65dc54e
Вероника Будюк <veronika.budyuk2004@gmail.com>
2024-04-01 23:05:22 +0400

    done add task

--- src/bintree.cc
+++ src/bintree.cc
@@ -1,14 +1,9 @@
 #include <bintree/bintree.h>
 
 
-Node::Node() {
-	data = 0;
-	left = nullptr;
-	right = nullptr;
-}
-
 Node::Node(int key) {
 	data = key;
+	count = 0;
 	left = nullptr;
 	right = nullptr;
 }
@@ -23,10 +18,7 @@ int Binary_tree::get_size() const
 	return _size;
 }
 
-Binary_tree::Binary_tree() {
-	_root = new Node();
-	_size = 1;
-}
+Binary_tree::Binary_tree(): _root(nullptr), _size(0) {}
 
 void Binary_tree::clear(Node* root){ 
 	if (!root) {
@@ -41,6 +33,8 @@ void Binary_tree::clear(Node* root){
 Binary_tree::Binary_tree(int key) {
 	_root = new Node(key);
 	_size = 1;
+	_root->count = 0;
+
 }
 
 Binary_tree::~Binary_tree() {
@@ -52,27 +46,33 @@ Binary_tree::~Binary_tree() {
 bool Binary_tree::insert(int key) {
 	if (!_root) {
 		_root = new Node(key);
+		_size++;
 		return true;
 	}
-
 	Node* tmp = _root;
-	while (tmp && tmp->data != key) {
-		
-		if (tmp->data > key) {
-			if (tmp->left)
+	while (tmp) {
+		if (tmp->data == key) {
+			tmp->count++;
+			++_size;
+			return true;
+		}
+		else if (tmp->data > key) {
+			if (tmp->left) {
 				tmp = tmp->left;
+			}
 			else {
 				tmp->left = new Node(key);
-				++_size;
+				_size++;
 				return true;
 			}
 		}
 		else {
-			if (tmp->right)
+			if (tmp->right) {
 				tmp = tmp->right;
+			}
 			else {
 				tmp->right = new Node(key);
-				++_size;
+				_size++;
 				return true;
 			}
 		}
@@ -80,21 +80,21 @@ bool Binary_tree::insert(int key) {
 	return false;
 }
 
-Node* Binary_tree::copy_tree(Node* root) {
-	insert(root->data);
-	if (root->left)
-		copy_tree(root->left);
-	if (root->right)
-		copy_tree(root->right);
-	return this->_root;
+Node* Binary_tree::copy_tree(Node* root, Node* other) {
+	if (!other)
+		return nullptr;
+
+	root = new Node(other->data);
+	root->count = other->count;
+	root->left = copy_tree(root->left, other->left);
+	root->right = copy_tree(root->right, other->right);
+	return root;
 }
 
 Binary_tree& Binary_tree::operator=(const Binary_tree& other) {
 	if (this != &other) {
 		clear(other._root);
-		if (other._root != nullptr)
-			_root = new Node(*other._root);
-		copy_tree(other._root);
+		copy_tree(_root, other._root);
 	}
 	return *this;
 }
@@ -102,8 +102,12 @@ Binary_tree& Binary_tree::operator=(const Binary_tree& other) {
 void Binary_tree::print_tree(Node* root) {
 	if (!root)
 		return;
+	cout << root->data;
+	if (root->count>0)
+		cout << "(" << root->count <<")" << " ";
+	cout << " ";
 	print_tree(root->left);
-	cout << root->data<<" ";
+	
 	print_tree(root->right);
 
 }
@@ -116,7 +120,8 @@ void Binary_tree::print() {
 
 Binary_tree::Binary_tree(const Binary_tree& other) {
 	_root = nullptr;
-	_root=copy_tree(other.get_root());
+	_size = other._size;
+	_root=copy_tree(_root, other.get_root());
 }
 
 bool Binary_tree::contains(int key) {
@@ -133,49 +138,80 @@ bool Binary_tree::contains(int key) {
 	return false;
 }
 
-bool Binary_tree::erase( int key) {
+int Binary_tree::count(int key) {
+	Node* tmp = _root;
+	while (tmp)
+	{
+		if (tmp->data == key)
+			return tmp->count;
+		if (tmp->data > key)
+			tmp = tmp->left;
+		else
+			tmp = tmp->right;
+	}
+	return -1;
+}
+
+bool Binary_tree::erase(int key) {
 	Node* tmp = _root;
 	Node* parent = nullptr;
-	while (tmp && tmp->data != key)
-	{
+	while (tmp && tmp->data != key) {
 		parent = tmp;
-		if (tmp->data > key)
-		{
+		if (tmp->data > key) {
 			tmp = tmp->left;
 		}
-		else
-		{
+		else {
 			tmp = tmp->right;
 		}
 	}
-	if (!tmp)
+	if (!tmp) {
 		return false;
-	if (!tmp->left)
-	{
-			
-		if (parent && parent->left == tmp)
-			parent->left = tmp->right;
-		if (parent && parent->right == tmp)
-			parent->right = tmp->right;
+	}
+
+	if (tmp->count > 0) {
+		tmp->count= tmp->count-1;
 		--_size;
-		delete tmp;
 		return true;
 	}
-	if (!tmp->right)
-	{
-			
-		if (parent && parent->left == tmp)
-			parent->left = tmp->left;
-		if (parent && parent->right == tmp)
-			parent->right = tmp->left;
+
+	if (!tmp->left) {
+		if (parent) {
+			if (parent->left == tmp) {
+				parent->left = tmp->right;
+			}
+			else {
+				parent->right = tmp->right;
+			}
+		}
+		else {
+			_root = tmp->right;
+		}
 		--_size;
 		delete tmp;
 		return true;
 	}
 
+	if (!tmp->right) {
+		if (parent) {
+			if (parent->left == tmp) {
+				parent->left = tmp->left;
+			}
+			else {
+				parent->right = tmp->left;
+			}
+		}
+		else {
+			_root = tmp->left;
+		}
+		_size--;
+		delete tmp;
+		return true;
+	}
+
 	Node* replace = tmp->right;
-	while (replace->left)
+	while (replace->left) {
 		replace = replace->left;
+	}
 	int replace_value = replace->data;
 	erase(replace_value);
 	tmp->data = replace_value;
